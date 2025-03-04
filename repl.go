977e8d0f55b896574a1f2bda626d1efd8fb3af5c
Kien.Nguyen <kien.nguyen@vipps.no>
2025-03-04 20:10:44 +0100

    explore

--- repl.go
+++ repl.go
@@ -14,10 +14,10 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(args ...string) error
 }
 
-type LocArea struct {
+type Loc struct {
 	Count    int
 	Next     string
 	Previous string
@@ -27,39 +27,68 @@ type LocArea struct {
 	}
 }
 
+type LocArea struct {
+	ID                   int
+	Name                 string
+	GameIndex            int
+	EncounterMethodRates []struct{} `json:"encounter_method_rates"`
+	Location             struct {
+		Name string
+		URL  string
+	}
+	Names             []struct{}
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string
+			URL  string
+		}
+		VersionDetails []struct{}
+	} `json:"pokemon_encounters"`
+}
+
 var commands map[string]cliCommand
 
 func main() {
-	loc := &LocArea{}
+	loc := &Loc{}
 	scanner := bufio.NewScanner(os.Stdin)
 	cache := internal.NewCache(time.Duration(time.Second * 5))
 	commands = map[string]cliCommand{
 		"exit": {
 			name:        "exit",
 			description: "Exit the Pokedex",
-			callback:    commandExit,
+			callback:    func(args ...string) error { return commandExit() },
 		},
 		"help": {
 			name:        "help",
 			description: "List all available commands",
-			callback:    commandHelp,
+			callback:    func(args ...string) error { return commandHelp() },
 		},
 		"map": {
 			name:        "map",
 			description: "Display the names of 20 location areas in the Pokemon world",
-			callback:    func() error { return commandMap(loc, cache) },
+			callback:    func(args ...string) error { return commandMap(loc, cache) },
 		},
 		"mapb": {
 			name:        "mapb",
 			description: "Display the names of Previous 20 location areas in the Pokemon world",
-			callback:    func() error { return commandMapb(loc, cache) },
+			callback:    func(args ...string) error { return commandMapb(loc, cache) },
 		},
 		"lenc": {
 			name:        "len_cache",
 			description: "Display the length of the cache",
-			callback: func() error {
+			callback: func(args ...string) error {
 				fmt.Println("Length of cache:---", len(cache.Items))
 				return nil
+			},
+		},
+		"explore": {
+			name:        "explore",
+			description: "Explore pokemons the location area",
+			callback: func(args ...string) error {
+				if len(args) != 1 {
+					return fmt.Errorf("exactly one location area name is required")
+				}
+				return commandExplore(args[0], cache)
 			},
 		},
 	}
@@ -72,10 +101,11 @@ func main() {
 			continue
 		}
 		if c, exists := commands[input[0]]; exists {
-			err := c.callback()
+			err := c.callback(input[1:]...)
 			if err != nil {
 				fmt.Println("Error executing command:", err)
 			}
+
 		} else {
 			fmt.Println("Unknown command.")
 		}
@@ -98,7 +128,7 @@ func commandHelp() error {
 	}
 	return nil
 }
-func commandMap(loc *LocArea, cache *internal.Cache) error {
+func commandMap(loc *Loc, cache *internal.Cache) error {
 	var url string
 	defer func() {
 		for _, r := range loc.Results {
@@ -117,7 +147,7 @@ func commandMap(loc *LocArea, cache *internal.Cache) error {
 		}
 		return nil
 	}
-	time.Sleep(time.Second * 2)
+	// time.Sleep(time.Second * 2)
 	res, err := http.Get(url)
 	if err != nil {
 		return err
@@ -135,7 +165,7 @@ func commandMap(loc *LocArea, cache *internal.Cache) error {
 
 	return nil
 }
-func commandMapb(loc *LocArea, cache *internal.Cache) error {
+func commandMapb(loc *Loc, cache *internal.Cache) error {
 	if loc.Previous == "" {
 		return nil
 	}
@@ -151,7 +181,7 @@ func commandMapb(loc *LocArea, cache *internal.Cache) error {
 		}
 		return nil
 	}
-	time.Sleep(time.Second * 2)
+	// time.Sleep(time.Second * 2)
 	res, err := http.Get(loc.Previous)
 	if err != nil {
 		return err
@@ -163,6 +193,41 @@ func commandMapb(loc *LocArea, cache *internal.Cache) error {
 	}
 	cache.Add(loc.Previous, body)
 	err = json.Unmarshal(body, loc)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func commandExplore(locationName string, cache *internal.Cache) error {
+
+	locArea := &LocArea{}
+	defer func() {
+		for _, p := range locArea.PokemonEncounters {
+			fmt.Println(p.Pokemon.Name)
+		}
+	}()
+	fmt.Println("Exploring location area", locationName, "...")
+	if val, exists := cache.Get(locationName); exists {
+		err := json.Unmarshal(val, locArea)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	res, err := http.Get("https://pokeapi.co/api/v2/location-area/" + locationName)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	cache.Add(locationName, body)
+	err = json.Unmarshal(body, locArea)
 	if err != nil {
 		return err
 	}
